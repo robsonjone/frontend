8d7652cada8a1d16bd61cbdc27070a4719f823bc
Robson Jone <robsonjone@hotmail.com>
2025-11-19 16:01:57 -0300

    Add Functions to add elementes into content and generate SQL

--- assets/js/qB.js
+++ assets/js/qB.js
@@ -35,6 +35,10 @@ const allTables={
 const queryToBuild = {}
 let currentElementDragged ;
 let currentTable = "";
+let currentField = "";
+
+let isOneField = false;
+let isOneTable = false;
 
 createTableList();
 
@@ -67,8 +71,11 @@ function addAllFieldsFromTableToList(table){
     const allFields = Object.keys(allTables[table]).filter((f)=>{
         return f !== "fk" && f !== "pk"
     })
-
-    console.log(`All Fields from table ${table}:`,allFields)
+    
+    allFields.map((field) => {
+        addOneFieldToList(table,field)
+    })
+    currentElementDragged = addAllFieldsFromTableToContent(allFields);
 
 }
 
@@ -76,27 +83,34 @@ function addAllFieldsFromTableToList(table){
  * * Add ALL SELECTED Fields to array list into query Object
  */
 function addAllSelectedFieldsToList(){
-    const selectedFields = {'endereco':["id","id_pessoa","nome_rua"]}
-    let table = Object.keys(selectedFields)[0]
+    
+    let table = currentTable
     
     //console.log(table)
-    let fieldsOfTable = selectedFields[table]
+    let fieldsOfTable = Array.from(document.querySelectorAll(".datalist[rel="+currentTable+"] .itemsDataList input[type=checkbox]:checked"))
+    let queryEditor = document.querySelector("#queryEditor")
     fieldsOfTable.map((field) => {
-        addOneFieldToList(table,field);
+        addOneFieldToList(table,field.parentNode.innerText);
+        queryEditor.appendChild(createElementScreen(field.parentNode.innerText))
     })
 }
 
 /**
  * * Add ONE Field to content into screen 
  */
-function addOneFieldToContent(){
-
+function addOneFieldToContent(field){
+    createElementScreen(field)
 }
 
 /**
  * * Add ALL Field to content on the screen 
  */
-function addAllFieldsFromTableToContent(){
+function addAllFieldsFromTableToContent(allFields){
+
+    const element = []
+    allFields.map((field) => element.push(createElementScreen(field)))
+
+    return element
 
 }
 
@@ -109,7 +123,7 @@ function addAllSelectedFieldsToContent(){
 
 /**
  * Create a list with tables and fields into screen
- */
+*/
 
 function createTableList(){
     const tableArray = Object.keys(allTables);
@@ -130,7 +144,11 @@ function createTableList(){
             event.dataTransfer.setData('text/html', event.target.nextElementSibling);
            
             currentTable = table.parentNode.attributes.rel.value
-            addAllFieldsFromTableToList(currentTable)
+            
+            isOneTable = true;
+            isOneField = false;
+
+            //addAllFieldsFromTableToList(currentTable)
 
             currentElementDragged = "" //addAllFieldsTable(event)
         });
@@ -167,9 +185,13 @@ function createTableList(){
                 event.dataTransfer.setData('text/html', event.target.nextElementSibling);
                 //console.log("DraggStart: currentTable")
                
+                isOneTable = false;
+                isOneField = true;
+                
                 currentTable = list.attributes.rel.value
+                currentField = field
                 currentElementDragged = createElementScreen(field)
-                addOneFieldToList(currentTable,field)
+                //addOneFieldToList(currentTable,field)
     
                 //let currentElementDragged = "" //addAllFieldsTable(event)
     
@@ -212,23 +234,43 @@ function createElementScreen(field){
     option.setAttribute("value","")
     option.innerText = "Selecione"
 
-    const option2 = option.cloneNode(true)
-    option2.setAttribute("value","max")
-    option2.innerText = "Maximo"
-
+    const agregFunctions = [
+        {value:"mix",label:"Mínimo"},
+        {value:"max",label:"Máximo"},
+        {value:"avg",label:"Médio"},
+        {value:"count",label:"Contar"},
+        {value:"sum",label:"Somar"},
+    ]
     select.insertAdjacentElement("beforeend",option)
-    select.insertAdjacentElement("beforeend",option2)
+
+    agregFunctions.map((func) => {
+        let opt = option.cloneNode(true)
+        opt.setAttribute("value",func.value)
+        opt.innerText = func.label
+        select.insertAdjacentElement("beforeend",opt)
+    })
+
 
     root.insertAdjacentElement("beforeend",select)
 
+    const toolbar = document.createElement("div");
+    toolbar.classList.add("actions")
+
+    const icon = document.createElement("i")
+    icon.classList.add("material-symbols-outlined","close");
+    icon.innerText = "close_small";
+    icon.addEventListener("click",(event) =>{
+        toolbar.parentNode.remove()
+    })
+
+    toolbar.appendChild(icon)
+
+    root.insertAdjacentElement("beforeend",toolbar);
     return root;
 }
 
 /**
  * Add drop Listener to Content
- */
-/**
- * Configure de content to drop all elements dragged
  */
 
 const queryEditor = Array.from(document.querySelectorAll('#queryEditor,#filterQueryEditor'));
@@ -250,10 +292,163 @@ queryEditor.map((qe)=>{
         event.preventDefault(); // Prevent default drop behavior
         const data = event.dataTransfer.getData('text/html');
 
-        console.log(currentTable)
-        if(currentElementDragged){
+        if(isOneTable){
+            addAllFieldsFromTableToList(currentTable)
+            currentElementDragged.map((element) => 
+                event.target.appendChild(element)
+            )
+        }
+
+        if(isOneField){
+            addOneFieldToList(currentTable,currentField)
             event.target.appendChild(currentElementDragged);
         }
+
+        console.log(currentTable)
+        console.log(currentElementDragged)
         qe.classList.remove('highlight'); 
     });
 });
+
+/**
+ * Add listener to add selected fields
+ */
+const addfield = Array.from(document.querySelectorAll('.addField'));
+
+addfield.map((add)=>{
+    add.addEventListener('click',function(){
+        //const fieldsSelected = Array.from(document.querySelectorAll(".itemsDataList > input[type='checkbox']:checked"));
+        //console.log(fieldsSelected[0].parentElement);
+        let table = add.parentElement.parentElement.attributes.rel.value;
+        currentTable = table
+        addAllSelectedFieldsToList();
+    })
+})
+
+/*
+    Build query from Object
+*/
+
+function createQUERY(){
+    let k = Object.keys(queryToBuild)
+    let fieldsSelected = []
+    let doubleFields = []
+    let q = "select "
+    let field = ""
+
+    k.map((key,pos) =>{
+        console.log(queryToBuild[key])
+        queryToBuild[key].map((field) =>{
+            if(fieldsSelected.includes(field)) {
+                doubleFields.push(field)
+            }
+            fieldsSelected.push(field)
+        })
+
+    })
+
+    let allFields = []
+    k.map((key,pos) =>{
+        queryToBuild[key].map((field) =>{
+            if(doubleFields.includes(field)) {
+                 field = key+"."+field
+            }
+            allFields.push(field)
+        })
+
+    })
+
+    console.log("Double Fields: ",allFields)
+    q += allFields.join(",")  
+    q += " from "+k.join(',')
+
+    console.log(queryToBuild)
+    console.log(q)
+}
+
+/*
+    Build query from input
+*/
+function createSQL(){
+    let fields = Array.from(document.querySelectorAll("#queryEditor > .selectFields:not(:first-child)"));
+    let query = "select ";
+    let select = [];
+    let from = [];
+    let where = [];
+    let groupBy = [];
+    let hasAgreg = false;
+
+    fields.map((field) =>{
+        let column = field.children[0].innerText 
+        let alias = field.children[1].value
+        let agreg = field.children[2].value
+
+        console.log("Agregacao: ",agreg);
+        
+        if(agreg !==""){
+            column = agreg+`(${column})`
+            hasAgreg = true;
+        }else{
+            if(hasAgreg)
+                groupBy.push(column);
+        }
+        if (alias !== "")
+            column += " as "+alias
+        select.push(column)
+    })
+
+    console.log("select "+select.join(',')," From ",sql.table.join(',')," group by ",groupBy.join(','));
+    if(select.length > 0)
+        query += select.join(',')
+    
+    query = query.replaceAll(",",",\n\t")
+    
+    if(sql.table.length > 0)
+        query += " from "+sql.table.join(',')
+
+    query = query.replace("from","\nfrom");
+
+    if(sql.table.length > 1){
+        let where = "\nwhere "
+        let hasFks = false;
+        
+        if(sql.table.length > 1) hasFks = true;
+
+        sql.table.map((table,pos) =>{
+            let foreignKeys  = allTables[table].fk;
+
+            foreignKeys.map((foreignKey,index)=>{
+                console.log("Index FK: ", "Table:",pos ," ",sql.table.length, "FK:",index," ",foreignKeys.length);
+                let filter = ""
+                if(foreignKey){
+                    filter = foreignKey.table+".id = "+table+"."+foreignKey.field+" \n";
+                }
+                if(hasFks && pos < sql.table.length){
+                    if(index < foreignKeys.length )
+                        filter = " and "+filter
+                }
+                
+                where += filter;
+                hasFks = true;
+
+            })
+            console.log("FK:",allTables[table].fk)
+
+        })
+
+        query += where +"\n";
+        
+    }
+
+
+    
+    if(groupBy.length > 0)
+        query += " group by "+groupBy.join(',')
+    
+        query = query.replace("group by","\ngroup by");
+
+    console.log("query: ",query)
+
+    const output = document.querySelector(".scriptOutput");
+    output.innerHTML = `<pre>${query}</pre>`;
+}
